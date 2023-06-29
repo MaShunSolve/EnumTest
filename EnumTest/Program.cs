9a02d688eed5989d2691f42ed536191c1a9dfbb4
eddy <eddyhsieh@mail.kway.com.tw>
2023-06-29 17:42:33 +0800

    加入專案檔案。

---
+++ EnumTest/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<MailTarget> mailTargets = new List<MailTarget>() { MailTarget.LI, MailTarget.GI };
+        if (mailTargets.IndexOf(MailTarget.LI) != -1)
+        {
+            Console.WriteLine("LI");
+        }
+        if (mailTargets.IndexOf(MailTarget.GI) != -1)
+        {
+            Console.WriteLine("GI");
+        }
+        if (mailTargets.IndexOf(MailTarget.U) != -1)
+        {
+            Console.WriteLine("U");
+        }
+    }
+    public enum MailTarget
+    {
+
+        /// <summary>
+        /// Local IT
+        /// </summary>
+        LI = 1,
+        /// <summary>
+        /// Globle IT
+        /// </summary>
+        GI = 2,
+        /// <summary>
+        /// User
+        /// </summary>
+        U = 3,
+        /// <summary>
+        /// All
+        /// </summary>
+        A = 4
+    }
+}
